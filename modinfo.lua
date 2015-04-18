148f3cecce16b8074cede3d50734a68e4e9e7e3d
oberones <oberones@gmail.com>
2015-04-18 16:14:58 -0400

    More region and spawn tweaks

--- modinfo.lua
+++ modinfo.lua
@@ -2,53 +2,55 @@
 ------------------------------------------------------------------------------------------------
 
 
---Meta information about the MOD along with any possible config options
+
 name = "FourTwenty"
 description = "Finally, a reason to survive in the wilderness."
 author = "Malevolent Gods"
 version = "1.5"
-
 forumthread = ""
+
+
 priority = 0.346962880
 dst_compatible = true
 all_clients_require_mod = true
 client_only_mod = false
 
+
 --This may change as the game is updated.
 api_version = 10
 
---Sets the icon our mod will use in the DST menu
+
+
 icon_atlas = "FourTwenty.xml"
 icon = "FourTwenty.tex"
+
 
 configuration_options =
 {
 	{
-		--Eventually these will need to be adjusted when we finally fix weed tree spawning
 		name = "weed_tree_regions",
-		label = "Weed Tree Regions",
+		label = "Weed Spawn Regions",
 		options =
 		{
-			{description = "Stone Biomes", data = 1},
-			{description = "SBs+Marsh", data = 2},
-			{description = "SBs+M+Forrest", data = 3},
+			{description = "Forests", data = 1},
+			{description = "Plains", data = 2},
+			{description = "Swamps", data = 3},
 			{description = "Everywhere", data = 4},
 		},
-		default = 4,
+		default = 1,
 	},
 
 	{
 		name = "weed_tree_rate",
-		label = "Weed Tree Rate",
+		label = "Weed Spawn Rate",
 		options =
 		{
-			{description = "Default again", data = 1.5},
-			{description = "Super Rare", data = 0.3},
-			{description = "Rare", data = 0.6},
-			{description = "Default", data = 1.4},
-			{description = "Common", data = 2.0},
-			{description = "Lots!", data = 3.3},
+			{description = "Rare", data = 0.02},
+			{description = "Uncommon", data = 0.04},
+			{description = "Common", data = .1},
+			{description = "Plentiful", data = .5},
+			{description = "Stoner Paradise", data = 1.5},
 		},
-		default = 1.4,
+		default = .1,
 	},
 }
--- modworldgenmain.lua
+++ modworldgenmain.lua
@@ -1,35 +1,56 @@
-GLOBAL.require("map/terrain")
-local weed_tree_rate = .5
-local weed_tree_regions = 2
+--Defines custom worldgen settings
 
+GLOBAL.require("map/terrain")
+
+local weed_tree_regions = (GetModConfigData("weed_tree_regions"))
+local weed_tree_rate = (GetModConfigData("weed_tree_rate"))
 
 
 if weed_tree_regions~=nil then
-	if weed_tree_regions>1 then
-		GLOBAL.terrain.rooms.BGGrass.contents.distributeprefabs.weed_tree = (weed_tree_rate/50)
-		GLOBAL.terrain.rooms.Plain.contents.distributeprefabs.weed_tree = (weed_tree_rate/80)
-		GLOBAL.terrain.rooms.BarePlain.contents.distributeprefabs.weed_tree = (weed_tree_rate/50)
-		print("Spawned Weed Trees in Plains and Grass Biomes")
-	end
-	if weed_tree_regions>2 then
-		GLOBAL.terrain.rooms.CrappyForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/20)
-		GLOBAL.terrain.rooms.Forest.contents.distributeprefabs.weed_tree = (weed_tree_rate/20)
-		GLOBAL.terrain.rooms.DeepForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/20)
+
+	if weed_tree_regions == 1 then
+		GLOBAL.terrain.rooms.CrappyForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/40)
+		GLOBAL.terrain.rooms.Forest.contents.distributeprefabs.weed_tree = (weed_tree_rate/80)
+		GLOBAL.terrain.rooms.DeepForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/40)
 		GLOBAL.terrain.rooms.CrappyDeepForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/20)
-		GLOBAL.terrain.rooms.BGForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/20)
+		GLOBAL.terrain.rooms.BGForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/60)
 		print("Spawned Weed Trees in Forest Biomes")
 	end
-	if weed_tree_regions>3 then
+	
+	if weed_tree_regions == 2 then
+		GLOBAL.terrain.rooms.BGGrass.contents.distributeprefabs.weed_tree = (weed_tree_rate/50)
+		GLOBAL.terrain.rooms.Plain.contents.distributeprefabs.weed_tree = (weed_tree_rate/50)
+		GLOBAL.terrain.rooms.BarePlain.contents.distributeprefabs.weed_tree = (weed_tree_rate/80)
+		GLOBAL.terrain.rooms.BGSavanna.contents.distributeprefabs.weed_tree = (weed_tree_rate/90)
+		print("Spawned Weed Trees in Plains and Grass Biomes")
+	end
+	
+	if weed_tree_regions == 3 then
+		GLOBAL.terrain.rooms.Marsh.contents.distributeprefabs.weed_tree = (weed_tree_rate/16)
+		GLOBAL.terrain.rooms.BGMarsh.contents.distributeprefabs.weed_tree = (weed_tree_rate/16)
+		GLOBAL.terrain.rooms.SlightlyMermySwamp.contents.distributeprefabs.weed_tree = (weed_tree_rate/16)
+		print("Spawned Weed Trees in the Swamp Biomes")
+	end
+
+	if weed_tree_regions == 4 then
+		GLOBAL.terrain.rooms.CrappyForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/40)
+		GLOBAL.terrain.rooms.Forest.contents.distributeprefabs.weed_tree = (weed_tree_rate/80)
+		GLOBAL.terrain.rooms.DeepForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/40)
+		GLOBAL.terrain.rooms.CrappyDeepForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/20)
+		GLOBAL.terrain.rooms.BGForest.contents.distributeprefabs.weed_tree = (weed_tree_rate/60)
+		GLOBAL.terrain.rooms.BGGrass.contents.distributeprefabs.weed_tree = (weed_tree_rate/50)
+		GLOBAL.terrain.rooms.Plain.contents.distributeprefabs.weed_tree = (weed_tree_rate/50)
+		GLOBAL.terrain.rooms.BarePlain.contents.distributeprefabs.weed_tree = (weed_tree_rate/80)
+		GLOBAL.terrain.rooms.BGSavanna.contents.distributeprefabs.weed_tree = (weed_tree_rate/90)
 		GLOBAL.terrain.rooms.BGNoise.contents.distributeprefabs.weed_tree = (weed_tree_rate/60)	
 		GLOBAL.terrain.rooms.Marsh.contents.distributeprefabs.weed_tree = (weed_tree_rate/16)
 		GLOBAL.terrain.rooms.BGMarsh.contents.distributeprefabs.weed_tree = (weed_tree_rate/16)
 		GLOBAL.terrain.rooms.SlightlyMermySwamp.contents.distributeprefabs.weed_tree = (weed_tree_rate/16)
-		GLOBAL.terrain.rooms.BGSavanna.contents.distributeprefabs.weed_tree = (weed_tree_rate/70)
 		GLOBAL.terrain.rooms.BGDirt.contents.distributeprefabs.weed_tree = (weed_tree_rate/80)
 		print("Spawned Weed Trees almost fucking everywhere!")
 	end
 else
-	print("weed_tree_regions is nil/null")
+	print("weed_tree_regions is null")
 end
 
 --Don't grow on the road, wood floor, carpet, or checker tiles
