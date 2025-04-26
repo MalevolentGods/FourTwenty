c9da192b75b7555146ddb36187d07f91a7f0614c
kerb3r0s <malevolentgods@gmail.com>
2025-04-26 15:37:03 -0400

    bump: version

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@
 name = "FourTwenty"
 description = "Finally, a reason to survive in the wilderness."
 author = "Malevolent Gods"
-version = "2.1.1"
+version = "2.1.2"
 
 -- TODO: Create forum thread
 forumthread = ""
